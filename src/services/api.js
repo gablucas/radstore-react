33a6e862f02b46f235749f9bf7e5316d2fe4db01
Gabriel Lucas <gabthedeveloper@gmail.com>
2023-05-18 18:03:41 -0300

    Mais ajustes

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,3 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: 'https://gablucas.github.io/jsonapi/radstore/'
 })
-
-export const addressApi = axios.create({
-  baseURL: 'https://viacep.com.br/ws/'
-})
